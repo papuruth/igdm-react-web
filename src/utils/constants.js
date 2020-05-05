04634c17544eb205cf838341fc1f46a2e3fdaf37
Papu Kumar <papuruth@gmail.com>
2020-05-05 18:37:35 +0530

    refactor: handle react routes and build optimizations

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,6 @@ export const URL_REGEX = new RegExp(
   /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/,
   'i',
 );
-console.log(process.env.NODE_ENV);
 export const API_BASE_URL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:3001'
   : 'https://igdm-react.herokuapp.com';
